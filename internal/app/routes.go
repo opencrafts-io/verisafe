27ee75f10aa30406164ad832f98343997eef46a1
Erick <hearteric57@gmail.com>
2025-07-11 22:42:10 +0300

    feat: permission crud operations and permission - role assignment done!

--- internal/app/routes.go
+++ internal/app/routes.go
@@ -13,6 +13,7 @@ func (a *App) loadRoutes() http.Handler {
 
 	auth := auth.NewAuthenticator(a.config, a.logger)
 	roleHandler := handlers.RoleHandler{Logger: a.logger}
+	permHandler := handlers.PermissionHandler{Logger: a.logger}
 
 	// ping handler
 	router.HandleFunc("GET /ping", handlers.PingHandler)
@@ -33,5 +34,17 @@ func (a *App) loadRoutes() http.Handler {
 	router.HandleFunc("PATCH /roles/{id}", roleHandler.UpdateRole)
 	router.HandleFunc("GET /roles/assign/{user_id}/{role_id}", roleHandler.AssignUserRole)
 	router.HandleFunc("DELETE /roles/revoke/{user_id}/{role_id}", roleHandler.RevokeUserRole)
+
+	// Permissions
+	router.HandleFunc("POST /permissions/create", permHandler.CreatePermission)
+	router.HandleFunc("GET /permissions/{id}", permHandler.GetPermissionByID)
+	router.Handle("GET /permissions", middleware.PaginationMiddleware(10, 100)(
+		http.HandlerFunc(permHandler.GetAllPermissions),
+	))
+	router.HandleFunc("GET /permissions/user/{id}", permHandler.GetAllUserPermissions)
+	router.HandleFunc("PATCH /permissions/", permHandler.UpdatePermission)
+	router.HandleFunc("GET /permissions/assign/{perm_id}/{role_id}", permHandler.AssignRolePermission)
+	router.HandleFunc("DELETE /permissions/revoke/{perm_id}/{role_id}", permHandler.RevokeRolePermission)
+
 	return router
 }
